3b6ad878fa4f1192dae007f0f652d48bccc37a28
eigengrau23 <59920198+eigengrau23@users.noreply.github.com>
2020-02-01 23:44:44 +0000

    Music System Created Together with Alarm Sound
    Music System Created Tested and Working. States needs to be assigned to the event when alarms go off plus another piece of music when the timer will start counting off.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,20 +13,59 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID INTERACTIVE_MUSIC = 3734989563U;
         static const AkUniqueID MAIN_REACTOR = 658170097U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace HAPPY_DAYS_ARE_OVER
+        {
+            static const AkUniqueID GROUP = 2017233915U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALARM_SYREN = 1353553046U;
+                static const AkUniqueID HAPPY_DAYS = 3498417625U;
+            } // namespace STATE
+        } // namespace HAPPY_DAYS_ARE_OVER
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace HAPPY_DAYS
+        {
+            static const AkUniqueID GROUP = 3498417625U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace HAPPY_DAYS
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID INTERACTIVE_MUSIC = 3734989563U;
     } // namespace BANKS
 
     namespace BUSSES
     {
+        static const AkUniqueID ALARM_SYREN = 1353553046U;
         static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID INTERACTIVE_MUSIC = 3734989563U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PLAYERS = 2188949101U;
+        static const AkUniqueID UI_PUZZLES = 2423156673U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
