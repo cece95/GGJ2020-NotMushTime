23fe0138dbbcf6017a235ac226e60d6836324a41
IshidaKuro <lewiswood27@googlemail.com>
2020-02-01 00:17:43 +0000

    Stuff Can Move Now
    It might not be pretty, but it works

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [Header("Player Variables")]
+    public int player = 0;
+    public float Horizontal;
+    public float Vertical;
+    public bool Red;
+    public bool Green;
+    public bool Yellow;
+    public bool Blue;
+    public Vector3 Speed = new Vector3 (0,0,0);
+
+    [Header("Linked Scripts")]
+    public Rigidbody rigidbody_ ;
+    public PlayerInput input_;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    //get which player we are
+
+     
+    }
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (rigidbody_.name.Contains("Mush"))
+        {
+            player = 1;
+            //if we are mush, then we are player one
+            Vertical = input_.P1Vertical;
+            Horizontal = input_.P1Horizontal;
+
+            Red = input_.P1Red;
+            Green = input_.P1Green;
+            Yellow = input_.P1Yellow;
+            Blue = input_.P1Blue;
+
+
+        }
+        else
+        {
+            player = 2;
+            //otherwise we are player 2
+            Vertical = input_.P2Vertical;
+            Horizontal = input_.P2Horizontal;
+
+            Red = input_.P2Red;
+            Green = input_.P2Green;
+            Yellow = input_.P2Yellow;
+            Blue = input_.P2Blue;
+        }
+
+        Speed.x = Horizontal;
+        Speed.y = Vertical;
+        //move the player based on the player input
+        rigidbody_.velocity.Set(input_.P1Horizontal, input_.P1Vertical, 0);
+        rigidbody_.transform.SetPositionAndRotation((rigidbody_.position + Speed), rigidbody_.rotation);
+        
+
+        //if the player is near a grabbable object, pick it up
+
+        if(Green) //green button will be used for interaction, anything in these brackets will be called when the green button is pressed 
+        {
+            
+        }
+
+        if (Red) // if the red button is being pressed for the player
+        {
+
+        }
+
+        if (Blue) // if the blue button is being pressed
+        {
+
+        }
+
+        if (Yellow) //if the yellow button is being pressed
+        {
+
+        }
+     }
+}
