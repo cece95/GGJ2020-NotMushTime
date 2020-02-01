9ef94ab65f0657a5eb29e58d777f580933c5c977
cece95 <cesaregri@gmail.com>
2020-02-01 23:05:42 +0000

    timers finished

--- Assets/Scripts/Timers.cs
+++ Assets/Scripts/Timers.cs
@@ -1,35 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Timers : MonoBehaviour
 {
     static float TOTAL_TIME = 180.0f;
     
     float timeLeft;
-    float testingTime;
+    float elapsedTime;
+
+    TextMeshProUGUI elapsedTimeText;
+    TextMeshProUGUI timeLeftText;
 
     // Start is called before the first frame update
     void Start()
     {
         timeLeft = TOTAL_TIME;
-        testingTime = 0.0f;
+        elapsedTime = 0.0f;
+
+        elapsedTimeText = transform.Find("ElapsedTime").GetComponentInChildren<TextMeshProUGUI>();
+        timeLeftText = transform.Find("TimeLeft").GetComponentInChildren<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         timeLeft -= Time.deltaTime;
-        testingTime += Time.deltaTime;
+        elapsedTime += Time.deltaTime;
+
+        timeLeftText.text = string.Format("{0:D2}:{1:D2}", Mathf.FloorToInt(timeLeft / 60), Mathf.FloorToInt(timeLeft % 60));
+        elapsedTimeText.text = string.Format("{0:D2}:{1:D2}", Mathf.FloorToInt(elapsedTime / 60), Mathf.FloorToInt(elapsedTime % 60));
 
         if (timeLeft < 0.0f)
-        {
-            GameOver();
+        { 
+            UnityEvent gameOver = new UnityEvent();
+            gameOver.Invoke();
         }
-    }
-
-    void GameOver()
-    {
-
     }
 }
