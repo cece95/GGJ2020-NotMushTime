a41f630b076fb7f7929cc15486def479233a6c71
cece95 <cesaregri@gmail.com>
2020-02-01 01:34:58 +0000

    added block movement, to be tested

--- Assets/Puzzles/Scripts/SlidingBox.cs
+++ Assets/Puzzles/Scripts/SlidingBox.cs
@@ -8,9 +8,12 @@ public class SlidingBox : MonoBehaviour{
     // block prefab
     GameObject block;
 
-    // 
-    List<GameObject> blocks = new List<GameObject>();
+    GameObject[,] blocks = new GameObject[3,3];
     List<Vector3> possiblePositions = new List<Vector3>();
+
+    //controls
+    [HideInInspector] public float P1Vertical;
+    [HideInInspector] public float P1Horizontal;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,7 @@ public class SlidingBox : MonoBehaviour{
         block = (GameObject) Resources.Load("prefabs/Block");
 
         // create a set of positions so we check that there's no repetition
+        int gridPos = 0;
         for (int i = -1; i < 2; i++)
         {
             for (int j = -1; j < 2; j++)
@@ -27,6 +31,7 @@ public class SlidingBox : MonoBehaviour{
             }
         }
 
+        // generate blocks in random postiions
         List<int> alreadyExtractedNumbers = new List<int>();
         int n;
         for (int i = 0; i < 8; i++)
@@ -39,14 +44,60 @@ public class SlidingBox : MonoBehaviour{
             alreadyExtractedNumbers.Add(n);
             Vector3 pos = possiblePositions[n];
             GameObject b = Instantiate(block, pos, Quaternion.identity);
-            b.GetComponentInChildren<TextMeshProUGUI>().text = i.ToString(); 
+            b.GetComponentInChildren<TextMeshProUGUI>().text = i.ToString();
+            
+            // save the blocks in the position matrix
+            int x = n % 3;
+            int y = n / 3;
+            blocks[x, y] = b;
         }
-
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        // get the coordinates of the empty cell
+        int x = 0, y = 0;
 
+        for(int i=0; i<3; i++) { 
+            for(int j=0; j<3; j++){
+                if (blocks[i,j] == null){
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        // move the only box that can move in that direction. If none can move, do nothing
+        if (P1Vertical > 0){
+            if (y < 2) {
+                blocks[x, y] = blocks[x, y + 1];
+                blocks[x, y + 1] = null;
+                blocks[x, y].transform.Translate(0, 3, 0);
+            }
+        }
+
+        if (P1Vertical < 0){
+            if (y > 0){
+                blocks[x, y] = blocks[x, y - 1];
+                blocks[x, y - 1] = null;
+                blocks[x, y].transform.Translate(0, -3, 0);
+            }
+        }
+
+        if (P1Horizontal > 0){
+            if (x > 0){
+                blocks[x, y] = blocks[x - 1, y];
+                blocks[x - 1, y] = null;
+                blocks[x, y].transform.Translate(3, 0, 0);
+            }
+        }
+
+        if (P1Horizontal < 0){
+            if (x < 2){
+                blocks[x, y] = blocks[x + 1, y];
+                blocks[x + 1, y] = null;
+                blocks[x, y].transform.Translate(-3, 0, 0);
+            }
+        }
     }
 }
