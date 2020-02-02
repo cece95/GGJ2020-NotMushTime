3526c52b662937ac50b8875efe191aae1fbffec1
Torantulino <40276179@live.napier.ac.uk>
2020-02-02 13:57:04 +0000

    Adds first stages of door and button functionality

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void OpenDoor()
+    {
+        animator.SetBool("Open", true);
+    }
+
+    public void CloseDoor()
+    {
+        animator.SetBool("Open", false);
+    }
+
+}
